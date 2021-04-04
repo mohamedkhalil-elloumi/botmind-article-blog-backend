634802a41f46dd35a5aebfb20bf984e1136695d0
Khalil Elloumi <mohamedkhalil.elloumi@ensi-uma.tn>
2021-04-05 00:28:08 +0100

    remove useless env

--- src/config/index.ts
+++ src/config/index.ts
@@ -5,11 +5,6 @@ dotenv.config();
 
 export default {
   PORT: process.env.PORT,
-  POSTGRES_HOST: process.env.POSTGRES_HOST,
-  POSTGRES_PORT: process.env.POSTGRES_PORT,
-  POSTGRES_USER: process.env.POSTGRES_USER,
-  POSTGRES_PASSWORD: process.env.POSTGRES_PASSWORD,
-  POSTGRES_DATABASE: process.env.POSTGRES_DATABASE,
   POSTGRES_URL: process.env.POSTGRES_URL,
   SECRET: process.env.SECRET,
 };

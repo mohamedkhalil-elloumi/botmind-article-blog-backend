fd72f06c835c24fe9e8e8ca8d161b0756e20292b
Khalil Elloumi <mohamedkhalil.elloumi@ensi-uma.tn>
2021-04-05 00:16:51 +0100

    #

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,8 +4,6 @@ import "reflect-metadata";
 dotenv.config();
 
 export default {
-  NODE_ENV: process.env.NODE_ENV,
-  APP_NAME: process.env.APP_NAME,
   PORT: process.env.PORT,
   POSTGRES_HOST: process.env.POSTGRES_HOST,
   POSTGRES_PORT: process.env.POSTGRES_PORT,
